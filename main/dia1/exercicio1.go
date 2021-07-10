9f4db3feae1e011439ccf7cae41e67caef911674
obacarol <oba.carol@gmail.com>
2021-07-10 12:22:30 -0300

    First steps

---
+++ main/dia1/exercicio1.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	var um, dois, tres int
+	um, dois, tres = 1, 2, 3
+	fmt.Printf("Um:%d \nDois:%d \nTrês:%d\n", um, dois, tres)
+}
---
+++ main/dia1/exercicio2.go
@@ -0,0 +1,12 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	a, b := 230, 27
+	soma := a + b
+	subtracao := a - b
+	fmt.Printf("A: %d \nB: %d \nSoma: %d \nSubtração: %d", a, b, soma, subtracao)
+}
